bc561609e6444a09d9063751c7dd43b3b640a0d9
Andrew Kungu <drewskungu@gmail.com>
2025-03-20 19:26:19 +0300

    Hard

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // code your solution here
+function saturdayFun(event="roller-skate"){
+    return (`This Saturday, I want to ${event}!`)
+
+}
+saturdayFun("batheMyDog")
+
+function mondayWork(event="go to the office"){
+    return (`This Monday, I will ${event }.`)
+
+}
+mondayWork("work from home")
+
+function wrapAdjective(val='*'){
+
+
+    let adjective  = function( text = "a hard worker"){
+    return `You are ${val}${text}${val}!`};
+    return adjective;
+
+    
+};
